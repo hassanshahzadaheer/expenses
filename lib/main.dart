9dc277fc1cacd7cfb40f010c117f280cbd2ce362
hassanshahzadaheer <hassanshahzadaheer@gmail.com>
2022-03-25 12:38:29 +0500

    styling transaction area

--- lib/main.dart
+++ lib/main.dart
@@ -4,54 +4,54 @@ import 'package:flutter/material.dart';
 void main() => runApp(Expenses());
 
 class Expenses extends StatelessWidget {
-
   final List<Transaction> transactions = [
-    Transaction(
-      'tr-1',
-      'T-shirt',
-      44.4,
-      DateTime.now()
-    ),
-     Transaction(
-      'tr-2',
-      'Laptop bag',
-      44.4,
-      DateTime.now()
-    ),
+    Transaction('tr-1', 'T-shirt', 44.4, DateTime.now()),
+    Transaction('tr-2', 'Laptop bag', 44.4, DateTime.now()),
   ];
-
 
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title : Text("Expenses Tracker"),
+          title: Text("Expenses Tracker"),
           backgroundColor: Colors.black,
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget> [
+          children: <Widget>[
             Card(
               color: Colors.cyan,
               shape: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide(color: Colors.black12)
-              ),
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.black12)),
               margin: EdgeInsets.all(10.0),
               child: Container(
                 width: double.infinity,
                 height: 40,
                 child: Text("Show text here"),
-                
               ),
               elevation: 8,
             ),
-            Card(
-              
-              child: Text("Transcation"),
+            Column(
+              children: transactions.map((transaction) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        child: Text(transaction.amount.toString()),
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text(transaction.name),
+                          Text(transaction.date.toString()),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
             ),
           ],
         ),
