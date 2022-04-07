8cb389d0dcc9b4fcc9fbe5705f3cd9963207e20f
hassanshahzadaheer <hassanshahzadaheer@gmail.com>
2022-04-07 11:56:48 +0500

    validation and aap design upgration

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -2,43 +2,60 @@ import 'package:flutter/material.dart';
 import 'package:expenses/widgets/transaction_list.dart';
 import 'package:expenses/widgets/user_transaction.dart';
 
-
 class NewTransaction extends StatelessWidget {
   final Function getTranscation;
   final inputTitle = TextEditingController();
   final inputAmount = TextEditingController();
 
-
   NewTransaction(this.getTranscation);
+
   
+
+  void submitData() {
+    final enteredTitle = inputTitle.text;
+    final enteredAmount = double.parse(inputAmount.text);
+    if (enteredTitle.isEmpty || enteredAmount <= 0)
+    {
+      return;
+    }
+    getTranscation(
+      enteredTitle,
+      enteredAmount,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Card(
       margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-              child: Container(
-                 decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
-                padding: EdgeInsets.all(3),
-                child: Column(children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Title',fillColor: Color.fromRGBO(64, 75, 96, .9),filled: true),
-                   style: TextStyle(color: Colors.yellowAccent, fontWeight: FontWeight.bold),
-                    controller: inputTitle,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Amount',fillColor: Color.fromRGBO(64, 75, 96, .9),filled: true),
-                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-                   controller: inputAmount,
-                  ),
-                  TextButton(onPressed: () {
-                    getTranscation (
-                      inputTitle.text,
-                      double.parse(inputAmount.text),
-                    );                   
-                  }, child: Text("Submit" , style: TextStyle(color: Colors.yellowAccent, fontWeight: FontWeight.bold),)),
-                ], crossAxisAlignment: CrossAxisAlignment.end),
-              ),
-               elevation: 8.0,
-            );
+      child: Container(
+        decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+        padding: EdgeInsets.all(10),
+        child: Column(children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: 'Title'),
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+            controller: inputTitle,
+            onSubmitted: (_) => submitData,
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'Amount'),
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+            controller: inputAmount,
+            keyboardType: TextInputType.number,
+            onSubmitted: (_) => submitData,
+          ),
+          TextButton(
+              onPressed: submitData,
+              child: Text(
+                "Submit",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              )),
+        ], crossAxisAlignment: CrossAxisAlignment.end),
+      ),
+      elevation: 8.0,
+    );
   }
 }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,53 +11,50 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
-      
-      height: 400,
-      child: ListView(
-        
-        children: transactions.map((transaction) {
-          return Card(
-            
-            elevation: 8.0,
-            child: Row(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                  decoration: new BoxDecoration(
-                      border: new Border(
-                          right: new BorderSide(
-                              width: 1.0, color: Colors.white24))),
-                  margin:      new EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
-                  child: Text(
-                    '\Rs ${transaction.amount}',
-                    style: TextStyle(
-                        color: Colors.redAccent, fontWeight: FontWeight.bold),
-                  ),
-
-                  
-                ),
-                Column(
-                  
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      transaction.name,
+        height: 400,
+        child: ListView.builder(
+          itemBuilder: (widgetItems, index) {
+            return Card(
+              margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+              elevation: 8.0,
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                    decoration: new BoxDecoration(
+                        border: new Border(
+                            right: new BorderSide(
+                                width: 1.0, color: Colors.white24))),
+                    margin: new EdgeInsets.symmetric(
+                        horizontal: 5.0, vertical: 5.0),
+                    child: Text(
+                      '\Rs ${transactions[index].amount.toStringAsFixed(2)}',
                       style: TextStyle(
-                          color: Colors.indigoAccent,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500),
+                          color: Colors.redAccent, fontWeight: FontWeight.bold),
                     ),
-                    Text(
-                      DateFormat('yyyy-MM-dd').format(transaction.date),
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          );
-        }).toList(),
-      ),
-    );
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        transactions[index].name,
+                        style: TextStyle(
+                            color: Colors.indigoAccent,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        DateFormat('yyyy-MM-dd').format(transactions[index].date),
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+          itemCount: transactions.length,
+        ));
   }
 }
